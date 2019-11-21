cd48c18aa97b1692dc9c649eb81b642f4a4a3e50
kana <tailei.yang@bangtuike.com>
2019-11-21 16:08:01 +0800

    打印log

--- vendor-track.js
+++ vendor-track.js
@@ -64,13 +64,13 @@ var brpop = function () {
                 var upload = pool_track.UploadEvent()
                 upload.watch(function (error, result) {
                     if (!error) {
-                        // console.log("dataid: " + result.args.dataid);
-                        // console.log("txhash: " + result.transactionHash);
+                        console.log("dataid: " + result.args.dataid)
+                        console.log("txhash: " + result.transactionHash)
                         axios.post('http://ums.proton.global/api/v1/track_node_call', {
                             txhash: result.transactionHash,
                             dataid: result.args.dataid
                         }).then(function (response) {
-                            // console.log(response);
+                            console.log(response)
                             upload.stopWatching()
                         }).catch(function (err) {
                             console.log(err)
